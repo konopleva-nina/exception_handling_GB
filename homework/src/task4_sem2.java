e4990bc5b2a9a6f759e824d99a3520d8bc75009f
Nina Konopleva <orangemise@gmail.com>
2023-07-29 19:13:25 +0300

    add homework seminar 2

---
+++ homework/src/task4_sem2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/*
+Разработайте программу, которая выбросит Exception,
+когда пользователь вводит пустую строку. Пользователю
+должно показаться сообщение, что пустые строки вводить нельзя.
+ */
+public class task4_sem2 {
+    public static void main(String[] args) {
+        prompt();
+    }
+    public static void prompt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите не пустую строку: ");
+        String line = scanner.nextLine();
+        if(!line.isEmpty()) {
+            System.out.println(line);
+            prompt();
+        }
+        else {
+            throw new RuntimeException("Пустые строки вводить нельзя.");
+        }
+    }
+}
